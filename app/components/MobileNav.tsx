ca3244c1e817eafbc04024b1b0f1b1824c281ecb
Only Jonesy <95600342+Andy-the-mmx@users.noreply.github.com>
2025-11-25 13:39:40 +1100

    Adding bottom-4 to the open mobile nav

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -110,7 +110,7 @@ export const MobNav = () => {
 
 const LinksOverlay = () => {
     return (
-        <nav className="fixed right-4 top-4 z-40 h-[calc(80vh_-_32px)] w-[calc(100%_-_32px)] overflow-hidden">
+        <nav className="fixed right-4 top-4 bottom-4 z-40 h-[calc(100vh_-_32px)] w-[calc(100%_-_32px)] overflow-hidden">
             <Logo />
             <LinksContainer />
             <FooterCTAs />
